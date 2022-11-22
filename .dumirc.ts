45c8655a4267a97df31115ec40782de487737e8e
Neil <1522674529@qq.com>
2022-11-23 01:30:36 +0800

    fix: wrong publicPath for gh-pages

--- .dumirc.ts
+++ .dumirc.ts
@@ -9,5 +9,7 @@ export default defineConfig({
   alias: {
     '@': path.resolve(__dirname, 'src'),
   },
-  base: '/dumi2-template/',
+  // base 只和路由有关，publicPath 影响资源文件的访问
+  // base 和 publicPath 的配置只在 build 时有效，umi dev 写死为 /，不可配
+  publicPath: '/dumi2-template/',
 });
